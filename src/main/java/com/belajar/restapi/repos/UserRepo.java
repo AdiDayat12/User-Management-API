88c81f47e52c94e379d7eebc0868a82fcb63e6fe
LinAn <adilinan72@gmail.com>
2024-08-19 01:22:28 +0300

    feat: implement UserService class with CRUD operations

--- src/main/java/com/belajar/restapi/repos/UserRepo.java
+++ src/main/java/com/belajar/restapi/repos/UserRepo.java
@@ -3,5 +3,5 @@ package com.belajar.restapi.repos;
 import com.belajar.restapi.models.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepos extends CrudRepository<User, Long> {
+public interface UserRepo extends CrudRepository<User, Long> {
 }
---
+++ src/main/java/com/belajar/restapi/services/UserService.java
@@ -0,0 +1,34 @@
+package com.belajar.restapi.services;
+
+import com.belajar.restapi.models.entities.User;
+import com.belajar.restapi.repos.UserRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    //if data exists in database then the data will be updated, if not then it will be created
+    public User create (User user) {
+        return userRepo.save(user);
+    }
+
+    public Iterable<User> findALl () {
+        return userRepo.findAll();
+    }
+
+    public User findOne (Long id) {
+        Optional<User> userOptional = userRepo.findById(id);
+        return userOptional.orElse(null);
+    }
+
+    public void delete (Long id) {
+        userRepo.deleteById(id);
+    }
+}
