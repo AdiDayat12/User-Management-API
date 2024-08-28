6d4a2b32bb5fcfbc218be33ab47c8c680eaff4fc
LinAn <adilinan72@gmail.com>
2024-08-28 16:14:54 +0300

    Add ModelMapper bean configuration in main application class
    Purpose: Configure ModelMapper bean in RestapiApplication to facilitate object mapping between DTOs and entities throughout the application.

--- src/main/java/com/belajar/restapi/RestapiApplication.java
+++ src/main/java/com/belajar/restapi/RestapiApplication.java
@@ -1,7 +1,12 @@
 package com.belajar.restapi;
 
+import com.belajar.restapi.dto.AddressDto;
+import com.belajar.restapi.models.entities.Address;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestapiApplication {
@@ -10,4 +15,18 @@ public class RestapiApplication {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.addMappings(new PropertyMap<AddressDto, Address>() {
+			@Override
+			protected void configure() {
+				map().setStreet(source.getStreet());
+				map().setCity(source.getCity());
+				map().setState(source.getState());
+				map().setZipCode(source.getZipCode());
+			}
+		});
+		return modelMapper;
+	}
 }
