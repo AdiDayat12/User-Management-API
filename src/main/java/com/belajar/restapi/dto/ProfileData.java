6df35a94e056d24eb9c1c1b2e0de25aad6bbd74a
LinAn <adilinan72@gmail.com>
2024-08-28 16:13:12 +0300

    Add ProfileData class for transferring profile data in API requests
    Purpose: Define a Data Transfer Object (DTO) for validating and transferring Profile data in API requests.

---
+++ src/main/java/com/belajar/restapi/dto/ProfileData.java
@@ -0,0 +1,37 @@
+package com.belajar.restapi.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDate;
+
+@Data
+public class ProfileData {
+
+
+    @NotBlank(message = "First name required")
+    private String firstName;
+
+    @NotBlank(message = "Last name required")
+    private String lastName;
+
+    private String bio;
+
+    @URL(message = "Avatar URL must be a valid URL")
+    private String avatarUrl;
+
+    @Pattern(regexp = "\\d{10}", message = "Phone must be a 10-digit number")
+    private String phone;
+
+    @Valid
+    private AddressDto address;
+
+
+    @Past(message = "Birth date must be in the past")
+    private LocalDate birthDate;
+}
