fae23770a3b8a008051b8a582d82433a1435e14e
LinAn <adilinan72@gmail.com>
2024-08-28 16:14:06 +0300

    Create AddressDto class for transferring address data in API requests
    Purpose: Define a Data Transfer Object (DTO) for transferring address data in API requests, facilitating data validation and mapping.

---
+++ src/main/java/com/belajar/restapi/dto/AddressDto.java
@@ -0,0 +1,12 @@
+package com.belajar.restapi.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AddressDto {
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+}
